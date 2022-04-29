c2b5f26e30753ec70a4e9c49055b2908790394ca
KingJavier <jerey62@gmail.com>
2022-04-29 15:48:51 -0500

    Front Proyecto

---
+++ src/app/pages/rolinvit/inicioinv/inicio.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+//importación de las rutas
+import { Router } from '@angular/router';
+//importación de los servicios de la api
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+  //llamdo de los datos url y name como dato tipo string
+  url: string;
+
+  nombre: string;
+
+  constructor(private router: Router, private usuarioServicio: TaskService) {
+    //llamdo del nombre del localstorage
+    this.nombre=localStorage.getItem('name');
+  }
+
+  ngOnInit() {
+    //llamdo de la foto
+    this.traerfotol();
+  }
+
+  salir(){
+    //función de cerrar seción
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+
+  traerfotol(){
+    //función de tarer la foto de los servicios de la api
+    const token = localStorage.getItem('token');
+    const idImgPerfil = localStorage.getItem('idImgPerfil');
+
+    console.log('Token -->',token);
+    console.log('imgPer -->',idImgPerfil);
+
+    this.usuarioServicio.traerfotoperfil(token, idImgPerfil).subscribe((res: any)=>{
+      console.log(res.data.url);
+      this.url = res.data.url;
+    });
+  }
+
+}
